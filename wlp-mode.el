7103034865c98c5dd9b94834940e6d0f6a5d0d51
Wilfred Hughes <me@wilfred.me.uk>
2020-12-28 22:19:38 -0800

    feat: Add a basic emacs mode for syntax highlighting

---
+++ wlp-mode.el
@@ -0,0 +1,52 @@
+;;; wlp-mode.el --- a major mode for Wilfred Lisp    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Wilfred Hughes
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A simple major mode for a crude lisp.
+
+;;; Code:
+
+;;;###autoload
+(add-to-list 'auto-mode-alist (cons "\\.wlp\\'" 'wlp-mode))
+
+(defvar wlp-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" table)
+
+    ;; Comments
+    (modify-syntax-entry ?\; "<" table)
+
+    ;; Newlines end comments.
+    (modify-syntax-entry ?\n ">" table)
+    
+    table))
+
+;;;###autoload
+(define-derived-mode wlp-mode prog-mode "WLisp"
+  "Major mode for editing Wilfred Lisp.
+
+\\{wlp-mode-map}"
+  (setq-local comment-start "; "))
+
+
+(provide 'wlp-mode)
+;;; wlp-mode.el ends here
