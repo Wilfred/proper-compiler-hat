4e99bc691b1b2e3938e11136f15feb548c0d7f45
Wilfred Hughes <me@wilfred.me.uk>
2021-01-18 23:59:41 -0800

    test: Add colour to output to distinguish headers

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,15 @@
 
 set -eu
 
+GREEN=$(tput setaf 2)
+WHITE=$(tput setaf 7)
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+
 for f in test/*.wlp; do
-    echo "==> $f"
+    echo -e "$BOLD$GREEN==>$WHITE ${f}$RESET"
     ./compiler.py "$f"
     ./hello
 done
