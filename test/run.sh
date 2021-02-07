d0d998181279a4e0e3f2f6fd445dd5dfce8c62b1
Wilfred Hughes <me@wilfred.me.uk>
2021-02-06 23:59:35 -0800

    test: Create a .stdout file if we don't already have one

--- test/run.sh
+++ test/run.sh
@@ -12,10 +12,21 @@ RESET=$(tput sgr0)
 for f in test/*.wlp; do
     exe=${f%.*} # strip .wlp
     echo -e "$BOLD$GREEN==>$WHITE ${f}$RESET"
+
+    # Compile the program.
     ./compiler.py "$f"
+
+    # Run the binary, and save the output.
     "./$exe" > "$f.out"
+    
     exit=$?
     rm "$exe"
+
+    # Create an empty .stdout file if it doesn't exist.
+    if [ ! -f "$f.stdout" ]; then
+        touch "$f.stdout"
+    fi
+    
     diff --color -u "$f.stdout" "$f.out"
     rm "$f.out"
     echo "exit code: $exit"
