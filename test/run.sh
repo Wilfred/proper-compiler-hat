349488fd1387b159f029be9cd10828dcd8c23b03
Wilfred Hughes <me@wilfred.me.uk>
2021-02-28 22:49:13 -0800

    feat: Allow tests to enforce what stderr is expected

--- test/run.sh
+++ test/run.sh
@@ -17,7 +17,7 @@ for f in test/*.wlp; do
     ./compiler.py "$f"
 
     # Run the binary, and save the output.
-    "./$exe" > "$f.out"
+    "./$exe" > "$f.out" 2> "$f.outerr"
     
     exit=$?
     rm "$exe"
@@ -26,9 +26,24 @@ for f in test/*.wlp; do
     if [ ! -f "$f.stdout" ]; then
         touch "$f.stdout"
     fi
-    
+
+    # Check that stdout matches what we expected.
     diff --color -u "$f.stdout" "$f.out"
+
+    # If we wrote anything to stderr, check it was what we expected.
+    if [ -s "$f.outerr" ]; then
+        # Create an empty .stderr if it doesn't exist.
+        if [ ! -f "$f.stderr" ]; then
+            touch "$f.stderr"
+        fi
+
+        diff --color -u "$f.stderr" "$f.outerr"
+    elif [ -s "$f.stderr" ]; then
+        echo "Expected something on stderr!"
+    fi
+
     rm "$f.out"
+    rm "$f.outerr"
 
     expected_exit=0
     if [ -f "$f.exitcode" ]; then
